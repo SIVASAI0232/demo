6a90ac3bb2e8cf1b7e9f398852aaa33c1616c7bc
SivaSai232 <ssvk232@gmail.com>
2020-07-21 23:12:51 +0530

    modified the run class

--- PhoneBookApp-1/src/main/java/com/sai/PhoneBookApp1Application.java
+++ PhoneBookApp-1/src/main/java/com/sai/PhoneBookApp1Application.java
@@ -2,12 +2,13 @@ package com.sai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class PhoneBookApp1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PhoneBookApp1Application.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(PhoneBookApp1Application.class, args);
 	}
 
 }
